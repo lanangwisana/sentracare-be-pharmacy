60e5c8326ca1e7816e95072f420cbaba6b34ca38
lanangwisana <lanangwisana@gmail.com>
2025-12-20 17:16:33 +0700

    Update code models.py

--- models.py
+++ models.py
@@ -12,20 +12,6 @@ class Obat(Base):
     stock = Column(Integer, default=0, nullable=False)
     price = Column(Float, nullable=False)
     description = Column(Text, nullable=True)
+    category = Column(String(50), nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-class Prescription(Base):
-    __tablename__ = "prescriptions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    patient_id = Column(Integer, nullable=False)
-    doctor_name = Column(String(100), nullable=False)
-    
-    # Ubah relasi ke tabel obat
-    obat_id = Column(Integer, ForeignKey("obat.id"), nullable=False) 
-    quantity = Column(Integer, nullable=False)
-    notes = Column(Text, nullable=True)
-    status = Column(String(20), default="PENDING")
-    created_at = Column(DateTime, default=datetime.utcnow)
-    obat = relationship("Obat")
